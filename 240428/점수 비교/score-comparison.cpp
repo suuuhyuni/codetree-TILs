ec19d3c99ef58c5cea38f150ff9c314780b2522e
suuuhyuni <163130794+suuuhyuni@users.noreply.github.com>
2024-04-28 18:02:00 +0900

    Title: 점수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240428/점수 비교/score-comparison.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int am, ae, bm, be;
+
+    cin>>am>>ae;
+    cin>>bm>>be;
+
+    cout<<((am>bm)&&(ae>be))<<endl;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
