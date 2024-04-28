51152d4a2150fc551bc78871c095863f7dd0a2de
suuuhyuni <163130794+suuuhyuni@users.noreply.github.com>
2024-04-28 15:49:55 +0900

    Title: 입력받아 연산하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240428/입력받아 연산하기/take-input-and-operate.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin>>a>>b;
+
+    a+=87;
+    b%=10;
+
+    cout<<a<<endl<<b;
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
