aa7322382751836d3240b8b9c4a27f72430ac183
suuuhyuni <163130794+suuuhyuni@users.noreply.github.com>
2024-04-28 17:06:14 +0900

    Title: 시력 검사 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240428/시력 검사 2/eye-test-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double i;
+
+    cin>>i;
+
+    if (i>=1.0)
+        cout<<"High";
+    else if (i>=0.5)
+        cout<<"Middle";
+    else
+        cout<<"Low";
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
