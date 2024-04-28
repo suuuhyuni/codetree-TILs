5921f6b203e9f23592858aad92deac84e4a2ba80
suuuhyuni <163130794+suuuhyuni@users.noreply.github.com>
2024-04-28 16:19:58 +0900

    Title: 큰 수에서 빼기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240428/큰 수에서 빼기/subtract-from-large-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin>>a>>b;
+
+    if (a>b){
+        cout<<a-b<<endl;
+    }
+    else{
+        cout<<b-a<<endl;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
