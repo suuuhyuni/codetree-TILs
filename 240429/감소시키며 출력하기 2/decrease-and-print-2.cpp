d6d1eb7b54bb68e44dcb7eac0579c06eb2db9a25
suuuhyuni <163130794+suuuhyuni@users.noreply.github.com>
2024-04-29 23:02:57 +0900

    Title: 감소시키며 출력하기 2, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240429/감소시키며 출력하기 2/decrease-and-print-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n=26;
+
+    while (n>=10){
+        cout<<n<<" ";
+        n--;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
