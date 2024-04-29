70fe2601e08c6194f8360edede1df1111086dccf
suuuhyuni <163130794+suuuhyuni@users.noreply.github.com>
2024-04-30 00:09:29 +0900

    Title: a/b 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240430/a/b 출력/a-divide-b.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, i;
+    double n;
+
+    cin>>a>>b;
+
+    cout<<a/b<<".";
+
+    a=a%b;
+
+    for (i=0;i<20;i++){
+        a = a * 10;
+        cout<<a/b;
+
+        a = a %
+    }
+    
+    
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
