18826abb8c8dfb39099145788f632c954e6f3e96
suuuhyuni <163130794+suuuhyuni@users.noreply.github.com>
2024-05-03 18:50:32 +0900

    Title: 369 게임, Time: 3ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240503/369 게임/369-game.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, i;
+
+    cin>>n;
+
+    for (i=1; i<=n; i++){
+        if ((i%3==0) || (i%10==3) || (i%10==6) || (i%10==9))
+            cout<<0;
+        else
+            cout<<i;
+        
+        cout<<" ";
+    }
+    
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
