c5d7efc9c324802307b48104310a97adfd2cd1d0
suuuhyuni <163130794+suuuhyuni@users.noreply.github.com>
2024-05-03 13:56:53 +0900

    Title: 자연수면 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240503/자연수면 출력/output-only-natural-number.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, i;
+    
+
+    cin>>a>>b;
+
+    if (a>0){
+        for (i=0;i<b;i++){
+            cout<<a;
+        }
+    }
+    else
+        cout<<0;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
