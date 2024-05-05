43ba2afbfbe56d95f17e0829da54922ca0981439
suuuhyuni <163130794+suuuhyuni@users.noreply.github.com>
2024-05-05 18:05:55 +0900

    Title: 청소당번 정하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240505/청소당번 정하기/cleaning-numbering.cpp
+++ 240505/청소당번 정하기/cleaning-numbering.cpp
@@ -9,31 +9,31 @@ int main() {
 
     for (i=1; i<=n; i++){
         if (i%2==0){
-
-            if (i%3==0){
-                cnt2++;
-            }
-
-            else if (i%12==0){
+            if (i%12==0){
                 cnt3++;
             }
-
-            else
+            else if (i%3==0){
+                cnt2++;
+            }
+            else{
                 cnt1++;
+            }
         }
 
         else if (i%3==0){
-
-            if (i%12==0)
+            if (i%12==0){
                 cnt3++;
-            else
+            }
+            else{
                 cnt2++;
+            }
         }
 
         else if (i%12==0){
             cnt3++;
         }
     }
+        
     cout<<cnt1<<" "<<cnt2<<" "<<cnt3;
 
     // 여기에 코드를 작성해주세요.
