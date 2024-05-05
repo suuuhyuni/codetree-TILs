5c11b81e4c0c8e2841fa88f40cf136cacbca91d0
suuuhyuni <163130794+suuuhyuni@users.noreply.github.com>
2024-05-05 18:19:25 +0900

    Title: 등장횟수 확인, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240505/등장횟수 확인/check-number-of-appearances.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, cnt=0, i;
+
+    for (i=0; i<5; i++){
+        cin>>n;
+
+        if (n%2==0)
+            cnt++;
+    }
+
+    cout<<cnt;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
