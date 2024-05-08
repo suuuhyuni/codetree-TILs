89632e0a0458a157cba86ebf818349989ec53fe1
suuuhyuni <163130794+suuuhyuni@users.noreply.github.com>
2024-05-09 01:45:23 +0900

    Title: 조건에 부합하는 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240509/조건에 부합하는 수/number-that-meets-the-condition.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+
+    cin>>a;
+
+    for (int i=1; i<=a; i++){
+
+        if (i%2 == 0 && i%4 != 0)
+            continue;
+        if ((i/8)%2==0)
+            continue;
+        if ((i%7)<4)
+            continue;
+        cout<<i<<" ";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
