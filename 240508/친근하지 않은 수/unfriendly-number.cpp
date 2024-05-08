5395e74c5e8637c8367f4d0500b8b1a949d4e36e
suuuhyuni <163130794+suuuhyuni@users.noreply.github.com>
2024-05-08 17:31:10 +0900

    Title: 친근하지 않은 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240508/친근하지 않은 수/unfriendly-number.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int n;
+
+    cin>>n;
+
+    int cnt=0;
+
+    for (int i=1; i<=n; i++){
+
+        if (i%2==0 || i%3==0 || i%5==0){
+            
+            continue;
+        }
+        cnt++;
+
+    }
+    cout<<cnt;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
