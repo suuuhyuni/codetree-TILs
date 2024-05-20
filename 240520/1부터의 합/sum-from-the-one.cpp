a457423c7fc071ead4e0be9101ca9423aca2510e
suuuhyuni <163130794+suuuhyuni@users.noreply.github.com>
2024-05-20 17:58:20 +0900

    Title: 1부터의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240520/1부터의 합/sum-from-the-one.cpp
+++ 240520/1부터의 합/sum-from-the-one.cpp
@@ -9,15 +9,13 @@ int main() {
 
     int sum=0;
 
-    for (int i=0; i<100; i++){
+    for (int i=1; i<=100; i++){
         sum+=i;
 
         if (sum>=n){
             cout<<i;
             break;
-        }
-
-           
+        }    
     }
     // 여기에 코드를 작성해주세요.
     return 0;
