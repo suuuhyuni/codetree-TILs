cf1659b01a244e035e9e4a60aa406ffa5917bc0b
suuuhyuni <163130794+suuuhyuni@users.noreply.github.com>
2024-07-27 21:37:31 +0900

    Title: 특정 위치의 문자 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240727/특정 위치의 문자 2/char-in-specific-location-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch[10];
+
+    for (int i=0; i<10; i++){
+        cin>>ch[i];
+    }
+
+    cout<<ch[1]<<" "<<ch[4]<<" "<<ch[7];
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
